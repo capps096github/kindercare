939062f7434473dc2fd79c3013b0fc72134ac5b5
C APPS GITHUB TEAM <84335028+capps096github@users.noreply.github.com>
2022-01-25 11:09:48 +0300

    Added some new Files

---
+++ web/src/kindercare.js
@@ -0,0 +1,20 @@
+function goToHome() {
+  setTimeout(function () {
+    location.href = "home.php";
+  }, 1000);
+}
+
+// * Go to Splash
+function goToSplash() {
+  location.href = "splash.php";
+}
+
+// * Go to Pupils Dashboard
+function goToPupilsDashboard() {
+  location.href = "./pupils/dashboard.php";
+}
+
+// * Go to Assignments Dashboard
+function goToAssignmentsDashboard() {
+  location.href = "./assignment/assignments.php";
+}
