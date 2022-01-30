5b1aafc84d69ab5ba4c7e096018e08a2fd7b5998
edwardtwahirwa8 <edwardtwahirwa8@gmail.com>
2022-01-30 16:39:59 +0300

    editted footer

--- web/src/pupils/register_pupil.php
+++ web/src/pupils/register_pupil.php
@@ -132,7 +132,7 @@ if (!isset($_SESSION["teacher_id"])) {
     <!-- Footer -->
     <footer class="mt-10 w-full bg-blue max-w-container mx-auto py-6 text-center space-y-2 items-center justify-center text-sm ">
       <p class="text-amber hover:text-white font-bold">
-        &copy; 2022 - KinderCare Learning Platform | All rights Reserved
+        &copy; 2022 - *KinderCare Learning Platform* | All rights Reserved
       </p>
 
     </footer>
