368f4ba2be06b2bbe08cca9b6f55163d63e01a88
SIMON <85620491+SimonDesire001@users.noreply.github.com>
2022-01-31 08:06:15 +0300

    Add files via upload
    Thats for auth of a pupil

--- cmd/main.c
+++ cmd/main.c
@@ -1,13 +1,93 @@
-#include <winsock.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mysql.h>
 
 
 
-int main()
+struct login{
+char fname[50];
+char lname[50];
+char username[50];
+char password[50];
+};
+registe()
 {
-    printf("Hello world!\n");
-MYSQL *con = mysql_init(NULL);
-       return 0;
+
+    FILE *log;
+    log=fopen("login.txt","w");
+    struct login l;
+
+
+    printf("Enter the first name: ");
+    scanf("%s", l.fname);
+
+    printf("Enter the last name: ");
+    scanf("%s", l.lname);
+
+    printf("Enter the  username: ");
+    scanf("%s", l.username);
+
+    printf("Enter the password: ");
+    scanf("%s", l.password);
+
+    fwrite(&l, sizeof(l),1,log);
+
+    fclose(log);
+
+    printf("\n\n your username is your \n");
+    printf("Now login with your UserID and Password\n");
+    printf("\n Press any key to continue......");
+    getch();
+    system("cls");
+     login();
+
+}
+login()
+{
+    char username[200],password[40];
+    FILE *log;
+    log=fopen("login.txt","r");
+    struct login l;
+    printf("Enter userID: ");
+    scanf("%s",&username);
+
+    printf("Enter password: ");
+    scanf("%s",&password);
+
+    while(fread(&l,sizeof(l),1,log))
+
+    {
+        if(strcmp(username,l.username)==0 && strcmp(password,l.password)==0)
+        {
+          printf("SUCCESSFUL LOGIN!!\n");
+        }
+
+        else
+        {
+            printf("PLEASE ENTER CORRECT USERID & PASSWORD!!!");
+        }
+    }
+   fclose(log);
+}
+
+main()
+{
+
+
+
+int cho;
+system("color 0b");
+
+printf("press '1' for registe\n  press '2' for login\n\n");
+scanf("%d",&cho);
+
+if(cho==1)
+  {
+    system("cls");
+    registe();
+  }
+else if(cho==2)
+  {
+    system("cls");
+    login();
+  }
 }
