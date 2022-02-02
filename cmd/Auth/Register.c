fdb771dcfc462748a10128c74f6af94edc6897b7
SIMON DESIRE <85620491+SimonDesire001@users.noreply.github.com>
2022-02-02 15:44:21 +0300

    Register

--- cmd/Auth/Register.c
+++ cmd/Auth/Register.c
@@ -6,7 +6,7 @@
 struct login{
 char fname[50];
 char lname[50];
-char username[50];
+char userid[50];
 char password[50];
 };
 registe()
@@ -24,8 +24,8 @@ FILE *fptr;
     printf("Enter the last name: ");
     scanf("%s", l.lname);
 
-    printf("Enter the  username: ");
-    scanf("%s", l.username);
+    printf("Enter the  userid: ");
+    scanf("%s", l.userid);
 
     printf("Enter the password: ");
     scanf("%s", l.password);
@@ -41,17 +41,17 @@ FILE *fptr;
     printf("\n Press any key to continue......");
     getch();
     system("cls");
-     login();
+    //  login();
 
 }
 login()
 {
-    char username[200],password[40];
+    char userid[200],password[40];
     FILE *log;
     log=fopen("login.txt","r");
     struct login l;
     printf("Enter userID: ");
-    scanf("%s",&username);
+    scanf("%s",&userid);
 
     printf("Enter password: ");
     scanf("%s",&password);
@@ -59,7 +59,7 @@ login()
     while(fread(&l,sizeof(l),1,log))
 
     {
-        if(strcmp(username,l.username)==0 && strcmp(password,l.password)==0)
+        if(strcmp(userid,l.userid)==0 && strcmp(password,l.password)==0)
 
         {
           printf("SUCCESSFUL LOGIN!!\n");
@@ -85,7 +85,7 @@ char lname[50];
     }
     
     
-     fprintf(fptr, "%s,", username);
+     fprintf(fptr, "%s,", userid);
      fprintf(fptr, "%s,", fname);
      fprintf(fptr, "%s,", lname);
 
