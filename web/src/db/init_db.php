905136e2378d7b1d5f706032b2bd2a4f6162c641
C APPS GITHUB TEAM <84335028+capps096github@users.noreply.github.com>
2022-02-06 00:45:38 +0300

    editted this for better app structuring

--- web/src/db/init_db.php
+++ web/src/db/init_db.php
@@ -32,28 +32,6 @@ $sql_pupils = "CREATE TABLE IF NOT EXISTS kindercare.pupils (
   `passwordx` VARCHAR(30) NOT NULL
 );";
 
-<<<<<<< Updated upstream
-// pupils data
-$sql_pupils_data = "INSERT IGNORE INTO kindercare.pupils(user_code,fname,lname,phone_no,gender,teacher_id,passwordx) VALUES
-('7823KLP','John','Mugisa','+256780955031','M','TR7823KLP','JM'),
-('2075KLP','Judith','Tusiime','+256700444642','F','TR2075KLP','JC'),
-('7853KLP','Mark','Desire','+256780958031','M','TR7893KLP','MD'),
-('2089KLP','Joan','Choosen','+256700744642','F','TR2045KLP','FC'),
-('7823KLP','Simon','Mugisa','+256780955031','M','TR7823KLP','SM'),
-('2075KLP','Jane','Martha','+256700444642','F','TR2075KLP','JM'),
-('7853KLP','John','Mugisah','+256380958031','M','TR7893KLP','JM'),
-('2099KLP','Ritah','Fibi','+256720744642','F','TR2045KLP','FC'),
-('7823KLP','Peter','Mugisa','+256780955031','M','TR7823KLP','PM'),
-('2075KLP','Jenny','Aine','+256700444642','F','TR2075KLP','JC'),
-('7853KLP','Mark','Desire','+256780958031','M','TR7893KLP','MD'),
-('2039KLP','Jane','Barbra','+256700744642','F','TR2045KLP','JC'),
-('7823KLP','Simon','Mugisa','+256780955031','M','TR7823KLP','SM'),
-('2075KLP','Patience','Martha','+256700444642','F','TR2075KLP','JM'),
-('7873KLP','John','Mugisa','+256380958031','M','TR7893KLP','JM'),
-('2019KLP','Ritah','Jane','+256720744642','F','TR2045KLP','RJ');";
-
-=======
->>>>>>> Stashed changes
 
 // assignments table
 $sql_assignments = "CREATE TABLE IF NOT EXISTS kindercare.assignments (
